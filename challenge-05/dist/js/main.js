ba91663ccf04ea3c848f67691da09b0a0b7bd268
sardarz <muhkween@gmail.com>
2021-01-12 17:04:52 +0300

    finished challenge 5

--- challenge-05/dist/js/main.js
+++ challenge-05/dist/js/main.js
@@ -1,14 +1,46 @@
+const data = [
+    {
+        name: 'Tanya Sinclair',
+        job: 'UX Engineer',
+        text: `  “ I’ve been interested in coding for a while but never taken the jump, until now. 
+        I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
+        excited about the future. ”`,
+        src: 'images/image-tanya.jpg',
+    },
+    {
+        name: 'John Tarkpor',
+        job: 'Junior Front-end Developer',
+        text: `  “ If you want to lay the best foundation possible I’d recommend taking this course. 
+        The depth the instructors go into is incredible. I now feel so confident about 
+        starting up as a professional developer. ”`,
+        src: 'images/image-john.jpg',
+    },
+];
 
-//   “ I’ve been interested in coding for a while but never taken the jump, until now. 
-//   I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-//   excited about the future. ”
+const fieldText = document.querySelector('.card__text');
+const fieldName = document.querySelector('.card__name');
+const fieldJob = document.querySelector('.card__job');
+const fieldImg = document.querySelector('.card__img');
+const prev = document.querySelector('.card__prev');
+const next = document.querySelector('.card__next');
 
-//   Tanya Sinclair
-//   UX Engineer
+let counter = 0;
 
-//   “ If you want to lay the best foundation possible I’d recommend taking this course. 
-//   The depth the instructors go into is incredible. I now feel so confident about 
-//   starting up as a professional developer. ”
+prev.addEventListener('click', () => {
+    counter--;
+    if (counter < 0) counter = data.length - 1;
+    changePerson();
+});
 
-//   John Tarkpor
-//   Junior Front-end Developer
+next.addEventListener('click', () => {
+    counter++;
+    if (counter >= data.length) counter = 0;
+    changePerson();
+});
+
+function changePerson() {
+    fieldText.innerHTML = data[counter].text;
+    fieldName.innerHTML = data[counter].name;
+    fieldJob.innerHTML = data[counter].job;
+    fieldImg.src = data[counter].src;
+}
